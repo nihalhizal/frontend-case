ac51cd9926ea815a4e97cb373a7923e363064bb8
Nihal Hızal <78216352+nihalhizal@users.noreply.github.com>
2024-10-30 15:21:59 +0300

    css clean code update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -40,38 +40,36 @@ const Footer: React.FC<FooterProps> = ({}) => {
 
   return (
     <footer>
-      <div className="flex w-full p-20 xs:p-16">
-        <div className="flex w-full justify-between text-lightBlue xs:flex-col xs:text-center xs:gap-20">
-          {data?.data?.map(({ title, items }: ColumnsData, index: number) => (
-            <div key={index} className="flex flex-col gap-4">
-              <div className="font-medium">{title}</div>
-              {items.map((item, itemIndex) => (
-                <div key={itemIndex}>
-                  <div>{item}</div>
-                </div>
-              ))}
-            </div>
-          ))}
-          <div className="flex flex-col gap-4">
-            <div className="font-medium">Get the App</div>
-            {appIcons.map((icon, index) => (
-              <div key={index}>
-                <button>
-                  <img src={icon.src} alt={icon.alt} />
-                </button>
+      <div className="footer-row p-20 xs:p-16 xs:gap-20">
+        {data?.data?.map(({ title, items }: ColumnsData, index: number) => (
+          <div key={index} className="flex flex-col gap-4">
+            <div className="font-medium">{title}</div>
+            {items.map((item, itemIndex) => (
+              <div key={itemIndex}>
+                <div>{item}</div>
               </div>
             ))}
-            <div className="font-medium">Follow Us</div>
-            <div className="flex gap-4 xs:justify-center">
-              {icons.map((Icon, index) => (
-                <button key={index}>{Icon}</button>
-              ))}
+          </div>
+        ))}
+        <div className="flex flex-col gap-4">
+          <div className="font-medium">Get the App</div>
+          {appIcons.map((icon, index) => (
+            <div key={index}>
+              <button>
+                <img src={icon.src} alt={icon.alt} />
+              </button>
             </div>
+          ))}
+          <div className="font-medium">Follow Us</div>
+          <div className="flex gap-4 xs:justify-center">
+            {icons.map((Icon, index) => (
+              <button key={index}>{Icon}</button>
+            ))}
           </div>
         </div>
       </div>
-      <div className="border-t mx-20 border-lightBlue opacity-25 xs:mx-4"></div>
-      <div className="flex w-full px-20 py-10 justify-between text-lightBlue xs:flex-col xs:px-8 xs:py-12 xs:text-center xs:gap-6">
+      <div className="divider"></div>
+      <div className="footer-row px-20 py-10 xs:px-8 xs:py-12 xs:gap-6">
         <div>Collers @ 2023. All rights reserved.</div>
         <div className="flex gap-8 xs:gap-6 xs:justify-center">
           <div>Terms</div>
